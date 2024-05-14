7fb0615d72208795ba2d917cf63844f01af36af5
Eva <eva.two.shoulder@gmail.com>
2024-05-14 13:15:36 +0800

    Change language dropdown

--- frontend/src/components/Global/Topbar.jsx
+++ frontend/src/components/Global/Topbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styles from "./Topbar.module.scss";
-import { Layout, Button } from "antd";
+import { Layout, Dropdown } from "antd";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import logo from "../../assets/logo.svg";
 import translation from "../../assets/translation.svg";
@@ -10,28 +10,38 @@ const { Header } = Layout;
 
 const Topbar = () => {
   const { i18n } = useTranslation();
-  const lngs = {
-    "zh-TW": { nativeName: "Taiwanese" },
-    "vi-VN": { nativeName: "Vietnamese" },
+  const items = [
+    {
+      key: "zh-TW",
+      label: "中文",
+    },
+    {
+      key: "vi-VN",
+      label: "Tiếng Việt",
+    },
+  ];
+  const onClick = ({ key }) => {
+    i18n.changeLanguage(key);
   };
+
   return (
     <Header>
       <div className={styles.inner}>
         <img src={logo} alt="隆廷實業股份有限公司" />
         <div className={styles.tools}>
-          <img src={translation} alt="語言切換" />
-          {Object.keys(lngs).map((lng) => (
-            <button
-              key={lng}
-              style={{
-                fontWeight: i18n.resolvedLanguage === lng ? "bold" : "normal",
-              }}
-              type="submit"
-              onClick={() => i18n.changeLanguage(lng)}
-            >
-              {lngs[lng].nativeName}
-            </button>
-          ))}
+          <Dropdown
+            menu={{
+              items,
+              selectable: true,
+              defaultSelectedKeys: ["3"],
+              trigger: ["click"],
+              onClick,
+            }}
+            placement="bottomLeft"
+          >
+            <img src={translation} alt="語言切換" />
+          </Dropdown>
+
           <NotificationsIcon sx={{ color: "#E0E7F5" }} />
         </div>
       </div>
