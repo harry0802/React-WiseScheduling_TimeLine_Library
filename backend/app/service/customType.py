38cabb8780fc4b82671bcf5bd06d5e175edf9ead
Eva <eva.two.shoulder@gmail.com>
2024-08-26 13:25:13 +0800

    Convert INT column type to TIMESTAMP type for datetime storage

--- backend/app/service/customType.py
+++ backend/app/service/customType.py
@@ -1,9 +1,44 @@
 from datetime import datetime
-from sqlalchemy.types import TypeDecorator, Integer
+from sqlalchemy.types import TypeDecorator, Integer, TIMESTAMP
 import pytz
 
 
 class TimestampDatetime(TypeDecorator):
+    """When storing datetime as TIMESTAMP(yyyy/MM/dd) type in MariaDB，use this class.
+
+    Args:
+        TypeDecorator (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    impl = TIMESTAMP
+    cache_ok = False
+
+    def process_bind_param(self, value, dialect):
+        # when inserting to database, convert ISO format datetime to UTC timezone
+        if value is not None:
+            return value.astimezone(pytz.timezone('UTC')).replace(tzinfo=None)
+        else:
+            return None
+        
+    def process_result_value(self, value, dialect):
+        # when selecting from database, convert unix timestamp to ISO format datetime
+        if value is not None:
+            return value
+        else:
+            return None
+        
+
+class IntegerDatetime(TypeDecorator):
+    """When storing datetime as Integer type in MariaDB，use this class to convert datetime to unix timestamp.
+
+    Args:
+        TypeDecorator (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     impl = Integer
     cache_ok = False
 
