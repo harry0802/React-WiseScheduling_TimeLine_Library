fc75343fd0d4df526b567b61151f32c37dd31778
Harry <f2eharry.two.shoulder@gmail.com>
2024-12-05 17:39:13 +0800

    fix: fix

--- frontend/src/components/QuotationManagementSystem/config/commonConfigs_v1.js
+++ frontend/src/components/QuotationManagementSystem/config/commonConfigs_v1.js
@@ -11,14 +11,14 @@ import { API_BASE } from "../../../store/api/apiConfig";
 
 //* API 端點配置
 const API_ENDPOINTS = {
-  MATERIAL_UNIT: `${API_BASE}/option/materialUnit`,
-  PACKAGING_UNIT: `${API_BASE}/option/packagingUnit`,
-  MACHINE_LIST: `${API_BASE}/machine/list`,
-  MACHINE_DETAIL: `${API_BASE}/machine`,
-  MATERIAL_PACKAGINGS: `${API_BASE}/material/packagings`,
-  MATERIAL_MATERIALS: `${API_BASE}/material/materials`,
-  MATERIAL_MATERIALUNITPRICE: `${API_BASE}/material/materialUnitPrice`,
-  MATERIAL_OPTION: `${API_BASE}/materialOption`,
+  MATERIAL_UNIT: `${API_BASE}option/materialUnit`,
+  PACKAGING_UNIT: `${API_BASE}option/packagingUnit`,
+  MACHINE_LIST: `${API_BASE}machine/list`,
+  MACHINE_DETAIL: `${API_BASE}machine`,
+  MATERIAL_PACKAGINGS: `${API_BASE}material/packagings`,
+  MATERIAL_MATERIALS: `${API_BASE}material/materials`,
+  MATERIAL_MATERIALUNITPRICE: `${API_BASE}material/materialUnitPrice`,
+  MATERIAL_OPTION: `${API_BASE}materialOption`,
 };
 
 //! =============== 2. 類型與介面 ===============
