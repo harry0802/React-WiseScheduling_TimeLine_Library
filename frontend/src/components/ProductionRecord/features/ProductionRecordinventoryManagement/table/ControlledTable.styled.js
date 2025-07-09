a66673b9b26a7382274e43a1666e89bf9ed27950
Harry <f2eharry.two.shoulder@gmail.com>
2025-07-09 09:18:14 +0800

    Fix: Enhance Ant Design table styling specificity
    Wrap global table styles in `&&{}` to ensure custom styles for headers, body cells, selection checkboxes, and pagination are correctly applied. This resolves issues where Ant Design's default styles might have overridden custom styling.

--- frontend/src/components/ProductionRecord/features/ProductionRecordinventoryManagement/table/ControlledTable.styled.js
+++ frontend/src/components/ProductionRecord/features/ProductionRecordinventoryManagement/table/ControlledTable.styled.js
@@ -43,54 +43,100 @@ export const ControlledTableActions = styled.div`
 // ControlledTable Antd 全局樣式覆蓋
 export const ControlledTableGlobalStyles = createGlobalStyle`
   .controlled-table-wrapper {
-    * {
-      border-radius: unset;
-    }
-
-    .ant-table-thead {
-      th.ant-table-cell {
-        border-top: 1px solid #8f8f8f;
-        border-bottom: 1px solid #8f8f8f;
-        color: #fff;
-        background: #186c98;
-        border-radius: unset !important;
-
-        &::before {
-          display: none;
-        }
-
-        &:first-child {
-          border-left: 1px solid #8f8f8f;
-        }
-
-        &:last-child {
-          border-right: 1px solid #8f8f8f;
-        }
-      }
-    }
-
-    .ant-table-pagination {
-      justify-content: center;
-      color: #8f8f8f;
-      margin-top: 88px;
-
-      a {
-        color: currentColor;
+    &&{
+      * {
+        border-radius: unset;
       }
 
-      .ant-pagination-item-active {
-        color: #fff;
-        border: unset;
-        background: #f39838;
-        border-radius: 50%;
+      .ant-table-thead {
+        th.ant-table-cell {
+          border-top: 1px solid #8f8f8f ;
+          border-bottom: 1px solid #8f8f8f;
+          color: #fff;
+          background: #186c98;
+          border-radius: unset !important;
+
+          &::before {
+            display: none;
+          }
+
+          &:first-child {
+            border-left: 1px solid #8f8f8f;
+          }
+
+          &:last-child {
+            border-right: 1px solid #8f8f8f;
+          }
+        }
+      }
+
+      .ant-table-tbody {
+        tr.ant-table-row {
+          td.ant-table-cell {
+            border-bottom: 1px solid #8f8f8f;
+            
+            &:first-child {
+              border-left: 1px solid #8f8f8f;
+            }
+
+            &:last-child {
+              border-right: 1px solid #8f8f8f;
+            }
+          }
+
+          &:hover {
+            td.ant-table-cell {
+              background: rgba(24, 108, 152, 0.1);
+            }
+          }
+        }
+      }
+
+      .ant-table-selection-column {
+        .ant-checkbox-wrapper {
+          .ant-checkbox {
+            .ant-checkbox-inner {
+              border-color: #8f8f8f;
+            }
+            
+            &.ant-checkbox-checked {
+              .ant-checkbox-inner {
+                background-color: #186c98;
+                border-color: #186c98;
+              }
+            }
+          }
+        }
+      }
+
+ .ant-pagination-item{
+  background: none;
+ }
+
+      .ant-table-pagination {
+        justify-content: center;
+        color: #8f8f8f;
+        margin-top: 88px;
+        background-color: none;
 
         a {
           color: currentColor;
         }
-      }
 
-      .ant-pagination-options .ant-select .ant-select-selector {
-        border-radius: 4px;
+        .ant-pagination-item-active {
+          color: #fff;
+          border: unset;
+          background: #f39838;
+          border-radius: 50%;
+
+          a {
+            color: currentColor;
+          }
+        }
+
+        .ant-pagination-options .ant-select .ant-select-selector {
+          border-radius: 4px;
+        }
       }
     }
   }
